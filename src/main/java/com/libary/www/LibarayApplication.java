07f84947f49ce7f43cb14d458c8bc4c4a68b2b7b
KHS-PC\lia04 <lia0417@naver.com>
2018-05-02 00:05:08 +0900

    start commit

---
+++ src/main/java/com/libary/www/LibarayApplication.java
@@ -0,0 +1,12 @@
+package com.libary.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarayApplication.class, args);
+	}
+}
